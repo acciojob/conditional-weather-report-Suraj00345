0bbc544cc3d33d9d277d246582094a5f6cc6443a
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2024-03-30 08:16:31 +0530

    5th commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,11 @@ import './../styles/App.css';
 const App = () => {
 
   const weather = { Temperature: 25, conditions: "Sunny"};
-  const temperatureColor = weather.temperature > 20 ? 'red' : 'blue';
+  const temperatureColor = weather.Temperature > 20 ? 'red' : 'blue';
   return (
     <div id = "main">
        <h2>Weather Report</h2>
-    <p>Temperature:<span style={{color:temperatureColor}}>{weather.temperature}</span></p>
+    <p>Temperature:<span style={{color:TemperatureColor}}>{weather.Temperature}</span></p>
     <p>Conditions: {weather.conditions}</p>
     </div>
   )
